c0a95c1dc2fd314bf3a5d3828cc6aa3a35d24019
konradjanica <konradjanica@outlook.com>
2016-05-29 08:37:45 +0930

    tweaking graph

--- unearthed/client/chart.js
+++ unearthed/client/chart.js
@@ -2,8 +2,7 @@ data = {
     labels: ["", "", "", "", ""],
     datasets: [
         {
-            label: "My First dataset",
-            backgroundColor: "rgba(255,99,132,0.2)",
+            backgroundColor: "rgba(255,99,132,1.0)",
             borderColor: "rgba(255,99,132,1)",
             borderWidth: 1,
             hoverBackgroundColor: "rgba(255,99,132,0.4)",
@@ -18,17 +17,13 @@ newBarChart = null;
 Template.barChart.onRendered(function() {
 
 var ctx = document.getElementById("resultBarChart").getContext("2d");
-
-    var options = {
-        legend: {
-            display: false,
-        }
-    };
+    Chart.defaults.global.legend.display = false;
+    Chart.defaults.bar.display = false;
+    Chart.defaults.global.defaultColor = "rgba(255,255,255,0.0)";
 
     newBarChart = new Chart(ctx, {
         type: 'bar',
         data: data,
-        options: options
     });
     console.log(data);
 });
