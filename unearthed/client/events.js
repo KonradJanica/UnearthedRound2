0855f0c7df37a1773da044c1b718415b339fc255
Ravi Hammond <ravihammond@gmail.com>
2016-05-29 00:58:48 +0930

    pictured now can be added infinitely

--- unearthed/client/events.js
+++ unearthed/client/events.js
@@ -18,3 +18,40 @@ Template.upload.events({
         })
     }
 });
+
+Template.image.rendered = function(){
+    (function() {
+        var $frame = $('#basic');
+        var $slidee = $frame.children('ul').eq(0);
+        var $wrap = $frame.parent();
+        var sly = new Sly($frame, {
+            horizontal: 1,
+            itemNav: 'basic',
+            smart: 1,
+            activateOn: 'click',
+            mouseDragging: 1,
+            touchDragging: 1,
+            releaseSwing: 1,
+            // startAt: 0,
+            scrollBar: $wrap.find('.scrollbar'),
+            scrollBy: 1,
+            pagesBar: $wrap.find('.pages'),
+            activatePageOn: 'click',
+            speed: 300,
+            elasticBounds: 1,
+            easing: 'easeOutExpo',
+            dragHandle: 1,
+            dynamicHandle: 1,
+            clickBar: 1,
+
+            // Buttons
+            forward: $wrap.find('.forward'),
+            backward: $wrap.find('.backward'),
+            prev: $wrap.find('.prev'),
+            next: $wrap.find('.next'),
+            prevPage: $wrap.find('.prevPage'),
+            nextPage: $wrap.find('.nextPage')
+        });
+        sly.init();
+    }());
+}
--- unearthed/client/sly.js
+++ unearthed/client/sly.js
@@ -4,43 +4,43 @@ jQuery(function($) {
     // -------------------------------------------------------------
     //   Basic Navigation
     // -------------------------------------------------------------
-    (function() {
-        var $frame = $('#basic');
-        var $slidee = $frame.children('ul').eq(0);
-        var $wrap = $frame.parent();
-        var sly = new Sly($frame, {
-            horizontal: 1,
-            itemNav: 'basic',
-            smart: 1,
-            activateOn: 'click',
-            mouseDragging: 1,
-            touchDragging: 1,
-            releaseSwing: 1,
-            // startAt: 0,
-            scrollBar: $wrap.find('.scrollbar'),
-            scrollBy: 1,
-            pagesBar: $wrap.find('.pages'),
-            activatePageOn: 'click',
-            speed: 300,
-            elasticBounds: 1,
-            easing: 'easeOutExpo',
-            dragHandle: 1,
-            dynamicHandle: 1,
-            clickBar: 1,
-
-            // Buttons
-            forward: $wrap.find('.forward'),
-            backward: $wrap.find('.backward'),
-            prev: $wrap.find('.prev'),
-            next: $wrap.find('.next'),
-            prevPage: $wrap.find('.prevPage'),
-            nextPage: $wrap.find('.nextPage')
-        });
-        sly.init();
+    // (function() {
+    //     var $frame = $('#basic');
+    //     var $slidee = $frame.children('ul').eq(0);
+    //     var $wrap = $frame.parent();
+    //     var sly = new Sly($frame, {
+    //         horizontal: 1,
+    //         itemNav: 'basic',
+    //         smart: 1,
+    //         activateOn: 'click',
+    //         mouseDragging: 1,
+    //         touchDragging: 1,
+    //         releaseSwing: 1,
+    //         // startAt: 0,
+    //         scrollBar: $wrap.find('.scrollbar'),
+    //         scrollBy: 1,
+    //         pagesBar: $wrap.find('.pages'),
+    //         activatePageOn: 'click',
+    //         speed: 300,
+    //         elasticBounds: 1,
+    //         easing: 'easeOutExpo',
+    //         dragHandle: 1,
+    //         dynamicHandle: 1,
+    //         clickBar: 1,
+    //
+    //         // Buttons
+    //         forward: $wrap.find('.forward'),
+    //         backward: $wrap.find('.backward'),
+    //         prev: $wrap.find('.prev'),
+    //         next: $wrap.find('.next'),
+    //         prevPage: $wrap.find('.prevPage'),
+    //         nextPage: $wrap.find('.nextPage')
+    //     });
+    //     sly.init();
         // sly.remove(0);
         // sly.remove(2);
 
         // Call Sly on frame
         //$frame.sly(k);
-    }());
+    // }());
 });
