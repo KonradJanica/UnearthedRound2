3d30669ea9860d6e30433f4165e9aac3d9e07c35
konradjanica <konradjanica@outlook.com>
2016-05-28 13:50:15 +0930

    pushed overlay

---
+++ unearthed/packages/meteor-noverlay-master/package.js
@@ -0,0 +1,22 @@
+Package.describe({
+	name: 'noverlay',
+	version: '0.1.0',
+	summary: 'Meteor nice overlay library for rendering templates in an overlay',
+	git: 'https://github.com/nelsyeung/meteor-noverlay.git',
+	documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+	api.versionsFrom('1.2.0.1');
+	api.use('ecmascript');
+	api.addFiles('lib/noverlay.css', 'client');
+	api.addFiles('lib/noverlay.js', 'client');
+	api.export('nOverlay', 'client');
+});
+
+Package.onTest(function(api) {
+	api.use('ecmascript');
+	api.use('tinytest');
+	api.use('nelsyeung:noverlay');
+	api.addFiles('test/noverlay-tests.js');
+});
